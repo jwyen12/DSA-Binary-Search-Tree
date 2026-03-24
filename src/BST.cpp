e2509702107663970d79018e1e4add85bb0a4dc6
jwyen12 <jakewyen1@gmail.com>
2026-03-24 15:28:21 -0400

    all functions added

--- src/BST.cpp
+++ src/BST.cpp
@@ -10,7 +10,7 @@ bool BST::insert(int val, TreeNode *&node){
     if(node->val > val){
         return insert(val, node->left);
     }
-    else if(node->val > val){
+    else if(node->val < val){
         return insert(node->val, node->right);
     }
     else{
@@ -81,3 +81,47 @@ int BST::height(){
     return height(root);
 }
 
+bool BST::remove(int val, TreeNode *&node){
+    if(node == nullptr){
+        return false;
+    }
+
+    if(node->val < val){
+        return remove(val, node->right);
+    }
+    else if(node->val > val){
+        return remove(val, node->left);
+    }
+    else{
+        if(node->right == nullptr && node->left == nullptr){
+            delete node;
+            node = nullptr;
+            return true;
+        }
+        else if(node->right == nullptr){
+            TreeNode *temp = node;
+            node = node->left;
+            delete temp;
+            return true;
+        }
+        else if(node->left ==nullptr){
+            TreeNode *temp = node;
+            node = node->right;
+            delete temp;
+            return true;
+        }
+        else{
+            TreeNode *candidate = node->left;
+            while(candidate->right != nullptr){
+                candidate = candidate->right;
+            }
+            node->val = candidate->val;
+            return remove(candidate->val, node->left);
+        }
+    }
+}
+
+
+bool BST::remove(int val){
+    return remove(val,root);
+}
